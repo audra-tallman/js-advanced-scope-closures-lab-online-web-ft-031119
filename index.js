f765de11d840cab5cc5a3bdb84a1155a1b8d2a3e
Audra Tallman <audi.tallman@gmail.com>
2019-06-25 18:41:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function produceDrivingRange( blockRange ) {
   return function ( startingBlock, endingBlock ) {
+
+  let start = parseInt( startingBlock );
+  let end = parseInt( endingBlock );
+  let distanceToTravel = Math.abs( end - start );
+  let difference = blockRange - distanceToTravel;
+  
